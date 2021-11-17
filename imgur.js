e7a7a9c49264afb674be4785a8e4176e98db6d16
piyushrana00456 <54103082+piyushrana00456@users.noreply.github.com>
2021-11-18 00:46:51 +0530

    add infinte scroll and data

--- imgur.js
+++ imgur.js
@@ -1,56 +1,70 @@
-const data = [
-  {
-    image: "./image/big.jpg",
-    title: "Storytime",
-    details: "Featured . 107,268 Posts",
-    color: "rgb(151,72,182)",
-  },
-  {
-    image: "./image/6.jpg",
-    title: "Metal",
-    details: "5,434 Posts",
-    color: "rgb(30,95,67)",
-  },
-  {
-    image: "Images/3.jpg",
-    title: "Football",
-    details: "13,027 Posts",
-    color: "rgb(170,51,230)",
-  },
-  {
-    image: "Images/4.jpg",
-    title: "Secret Santa",
-    details: "50,306 Posts",
-    color: "rgb(36,86,94)",
-  },
-  {
-    image: "Images/5.jpg",
-    title: "Steve Irwin",
-    details: "496 Posts",
-    color: "rgb(129,176,87)",
-  },
-  {
-    image: "Images/2.jpg",
-    title: "Inspiring",
-    details: "84,759 Posts",
-    color: "rgb(80,83,90)",
-  },
-  {
-    image: "Images/7.png",
-    title: "Family",
-    details: "7,543 Posts",
-    color: "rgb(21,149,89)",
-  },
-  {
-    image: "Images/1.jpg",
-    title: "Cat",
-    details: "260,934 Posts",
-    color: "rgb(46,167,153)",
-  },
-  {
-    image: "Images/9.png",
-    title: "Food",
-    details: "75,514 Posts",
-    color: "rgb(59,163,177)",
-  },
-];
+let data = [];
+let page = 1;
+
+const fetchData = async (page) => {
+  try {
+    let req = await fetch(
+      `https://api.unsplash.com/search/photos?page=${page}&per_page=30&query=L&client_id=WdleZc1GO2SvZj7EPJ0jE3frFtuId8CkHLM9UiVB3iw`
+    );
+    let res = await req.json();
+    console.log(res);
+    data = [...data, ...res.results];
+
+    console.log("data:", data);
+    appendData();
+  } catch (error) {
+    console.log("error:", error);
+  }
+};
+fetchData();
+
+function appendData() {
+  data.forEach((el) => {
+    let div = document.createElement("div");
+    div.classList.add("col");
+    div.innerHTML = `<div class="contain"></div>
+    <div class="randomImg">
+      <img src=${el.urls.small} alt="">
+    </div>
+    <div class="details">
+      <h3>${el.alt_description}</h3>
+      <div class="views">
+        
+      <span>${el.likes} <i class="fas fa-long-arrow-alt-down"></i></span>
+      <span>30 <i class="fas fa-comment-alt"></i></span
+      <span>${el.height} <i class="fas fa-eye"></i></span
+      </div>
+    </div>`;
+    document.querySelector(".cards").append(div);
+  });
+}
+appendData();
+
+window.addEventListener("scroll", function () {
+  let footer = document.querySelector(".footer");
+  let cards = document.querySelector(".cards");
+  footer.classList.toggle("sticky", window.scrollY);
+});
+
+const handleScroll = () => {
+  const html = document.documentElement;
+  const body = document.body;
+  const windowHeight =
+    "innerHeight" in window ? window.innerHeight : html.offsetHeight;
+
+  const docHeight = Math.max(
+    body.scrollHeight,
+    body.offsetHeight,
+    html.clientHeight,
+    html.scrollHeight,
+    html.offsetHeight
+  );
+  const windowBottom = windowHeight + window.pageYOffset;
+  if (windowBottom >= docHeight) {
+    page += 1;
+
+    fetchData(page);
+  }
+};
+
+window.addEventListener("scroll", handleScroll);
