ca8d6bf3b56bbe9491f382067f959d24b7b4f470
piyushrana00456 <54103082+piyushrana00456@users.noreply.github.com>
2021-11-17 23:06:17 +0530

    navbar css done

---
+++ imgur.js
@@ -0,0 +1,56 @@
+const data = [
+  {
+    image: "./image/big.jpg",
+    title: "Storytime",
+    details: "Featured . 107,268 Posts",
+    color: "rgb(151,72,182)",
+  },
+  {
+    image: "./image/6.jpg",
+    title: "Metal",
+    details: "5,434 Posts",
+    color: "rgb(30,95,67)",
+  },
+  {
+    image: "Images/3.jpg",
+    title: "Football",
+    details: "13,027 Posts",
+    color: "rgb(170,51,230)",
+  },
+  {
+    image: "Images/4.jpg",
+    title: "Secret Santa",
+    details: "50,306 Posts",
+    color: "rgb(36,86,94)",
+  },
+  {
+    image: "Images/5.jpg",
+    title: "Steve Irwin",
+    details: "496 Posts",
+    color: "rgb(129,176,87)",
+  },
+  {
+    image: "Images/2.jpg",
+    title: "Inspiring",
+    details: "84,759 Posts",
+    color: "rgb(80,83,90)",
+  },
+  {
+    image: "Images/7.png",
+    title: "Family",
+    details: "7,543 Posts",
+    color: "rgb(21,149,89)",
+  },
+  {
+    image: "Images/1.jpg",
+    title: "Cat",
+    details: "260,934 Posts",
+    color: "rgb(46,167,153)",
+  },
+  {
+    image: "Images/9.png",
+    title: "Food",
+    details: "75,514 Posts",
+    color: "rgb(59,163,177)",
+  },
+];
